f3833dbd778b8b4916dede7a37cba503f5f9f0ad
Juan José Gálvez Molinero <juanjosegalvezmolinero@hotmail.com>
2022-09-17 21:15:34 +0200

    Primera subida de proyecto

---
+++ src/test/java/com/jjgalvez/invetory/InvetoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjgalvez.invetory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvetoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
